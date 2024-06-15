83959b3cf52bf87768bf6458b7060d0f9e3b6828
Daris Fikri Zhalifunnas <127469235+Ardaza@users.noreply.github.com>
2024-06-16 04:07:32 +0700

    remove inventory :(

--- Dukun_Project/Assets/TestInventScritp/ItemSo.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
-public class ItemSo : ScriptableObject
-{
-    public Sprite itemIcon;
-    public string itemID;
-    public string itemName;
-}
